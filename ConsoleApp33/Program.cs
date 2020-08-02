3e0f5b2327cf93c3c8e64714877422b7d4fe5994
andy99ge <zjdecjtu@163.com>
2020-08-02 16:11:57 +0800

    testing some c# files

--- ConsoleApp33/Program.cs
+++ ConsoleApp33/Program.cs
@@ -7,6 +7,8 @@ namespace ConsoleApp33
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            Console.WriteLine("test");
         }
     }
 }
